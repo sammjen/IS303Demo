d76538351efc2181b26430d7aefb82a17921b45c
GavinClifton <166751534+Gavincli@users.noreply.github.com>
2025-02-20 10:27:59 -0700

    Created a function to multiply two given numbers
    Takes 2 numbers and multiplies them.

---
+++ multiply.py
@@ -0,0 +1,8 @@
+# Gavin Clifton
+# 20 February 2025
+# take input and multiply
+
+# Takes 2 given numbers and returns the product
+def multiplyTwoNumbers(num1, num2):
+    result = num1 * num2
+    return result
